00ebba7e219bde0a86007731e42870aff75b9a77
DAEUN28 <acone1128@gmail.com>
2019-12-14 11:34:32 +0900

    Create project

---
+++ Mongli/Mongli/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mongli
+//
+//  Created by baby1234 on 2019/12/14.
+//  Copyright © 2019 daeun28. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
