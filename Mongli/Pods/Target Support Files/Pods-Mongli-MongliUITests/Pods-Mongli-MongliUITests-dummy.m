a9030e0f7a8821e3bd667f7b507ecab91b435d0c
DAEUN28 <acone1128@gmail.com>
2020-02-03 15:03:34 +0900

    Delete all project

--- Mongli/Pods/Target Support Files/Pods-Mongli-MongliUITests/Pods-Mongli-MongliUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Mongli_MongliUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Mongli_MongliUITests
-@end
--- Mongli/Pods/Target Support Files/Pods-Mongli/Pods-Mongli-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Mongli : NSObject
-@end
-@implementation PodsDummy_Pods_Mongli
-@end
