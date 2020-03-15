22fe4f929042f72fba6e3ce4fb268c8dca418010
DAEUN28 <acone1128@gmail.com>
2020-03-15 21:27:02 +0900

    Add project
    Add pod setting

---
+++ Mongli/Mongli/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mongli
+//
+//  Created by DaEun Kim on 2020/03/15.
+//  Copyright © 2020 DaEun Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
